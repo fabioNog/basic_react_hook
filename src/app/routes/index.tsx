46a70a0e67a4c9c7170991c6340d5dcd18dc0a98
fabioNog <fabio.nogueira.carmo@gmail.com>
2022-08-12 10:54:12 -0300

    Create main route

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,17 +1,24 @@
+import path from "path";
 import ReactDOM from "react-dom/client";
+
 import {
     BrowserRouter,
     Routes,
     Route,
+    Navigate 
 } from "react-router-dom";
+import { Dashboard } from "../pages/Dashboard";
 // import your route components too
 
 export const RoutesApp = () => {
     return(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<h1>Ola</h1>}>
-            </Route>
+            <Route path="/"  element={<Dashboard/>}/>
+            <Route
+        path="*"
+        element={<Navigate to="/" replace />}
+    />
         </Routes>
     </BrowserRouter>
     )
